231a496258a4cd509e9115efa8812cc8ac71b365
Atul Oak <42930885+AtulOak@users.noreply.github.com>
2020-03-19 12:21:49 +0530

    Update P16_61.C

--- P16_61.C
+++ P16_61.C
@@ -1,14 +1,15 @@
+// This program is developed by Prof. Atul Oak
 #include<stdio.h>
-#include<conio.h>
+#include<conio.h>		// header files
 
 int main( )
 {
 
-	int n,s,d,n1,i;
+	int n,s,d,n1,i;		// variables to store data
 //	clrscr();
 	s = 0;
 	printf("\n\n\t -- Program for Armstrong Number --");
-	for(i=1; i<=1000; i++)
+	for(i=1; i<=1000; i++)		// check numbers from 1 to 1000
 	{
 	n = i;
 	s=0;
@@ -19,7 +20,7 @@ int main( )
 		n = n/10;
 	}
 	if(s == i)
-		printf("\n\n\t %d >> Armstrong Number",i);
+		printf("\n\n\t %d >> Armstrong Number",i);	// print Armstrong Number
 	}
 	getch();
 	return 0;
