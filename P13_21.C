2996c4a9c60ffe362b4e8eebc926eebd60286f04
Atul Oak <42930885+AtulOak@users.noreply.github.com>
2020-03-19 12:04:30 +0530

    Add files via upload

---
+++ P13_21.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main( )
+{
+	int n,i,j,k,sn = 1;
+	float t, s = 0.0;
+//	clrscr();
+	printf("\n\n\t -- sum of series 1/2 - 3/4 + 5/6....+ n");
+	printf("\n\n\t Pl. enter n > ");
+	scanf("%d", &n);
+	for(i=1,j=2,k=1; k<=n; i=i+2,j=j+2,k++)
+
+	{
+		t = (float)i/j;
+		t =  t * sn;
+		s = s + t;    // loop  to generate term & add
+		sn = sn * (-1);
+
+
+	}
+	printf("\n\n\t sum = %f", s);
+	getch();
+	return 0;
+}
